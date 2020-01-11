526c45816a7ab286b0d34dd325d91ae5d2ed3dbd
neeraj <neerajgupta2407@gmail.com>
2020-01-11 13:58:07 +0530

    Working: Budget Manager.

---
+++ budget_manager/options.js
@@ -0,0 +1,32 @@
+$(function () {
+    chrome.storage.sync.get('limit', function (budget) {
+        $('#limit').val(budget.limit);
+    })
+
+
+    $("#saveLimit").click(function () {
+        var limit = $('#limit').val();
+        if (limit){
+            chrome.storage.sync.set({'limit':limit}, function () {
+                close();
+            })
+        }
+    });
+
+    $("#resetTotal").click(function () {
+
+        chrome.storage.sync.set({'total':0}, function () {
+
+            var notificationObj = {
+                type:"basic",
+                title: "Total Reset.",
+                message: "Total amount has been reset to 0",
+                iconUrl: "images/icon48.png"
+            }
+            // console.log(notificationObj)
+            chrome.notifications.create('resetNotif', notificationObj)
+        })
+    });
+
+
+});
